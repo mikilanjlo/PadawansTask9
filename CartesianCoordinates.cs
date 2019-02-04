13d983961b49b367e18859f7c43547541de0ab9a
EPAM RD NET Lab, Minsk <31510338+EPM-RD-NETLAB@users.noreply.github.com>
2019-02-04 17:12:18 +0300

    Add project files.

---
+++ CartesianCoordinates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PadawansTask9
+{
+    public class CartesianCoordinates
+    {
+        public static List<Point> GetNeighbors(Point point, int range, params Point[] points)
+        {
+            // put your code here
+            throw new NotImplementedException();
+        }
+    }
+}
