fb8b41cc90436c290d1acb7e161dd5d1f2865b1e
mikilanjlo <mikilanjlo@yandex.by>
2019-02-22 11:13:44 +0300

    calculate

--- CartesianCoordinates.cs
+++ CartesianCoordinates.cs
@@ -7,8 +7,20 @@ namespace PadawansTask9
     {
         public static List<Point> GetNeighbors(Point point, int range, params Point[] points)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (point.Equals(null))
+                throw new ArgumentNullException();
+            if (range < 0)
+                throw new ArgumentException();
+            List<Point> result = new List<Point>();
+            GetNeighbors(point, range,  result, points);
+            return result;
+        }
+
+        private static void GetNeighbors(Point neighbor, int range, List<Point> result, params Point[] points)
+        {
+            foreach (Point point in points)
+                if (Math.Abs(neighbor.X - point.X) <= range && Math.Abs(neighbor.Y - point.Y) <= range)
+                    result.Add(point);
         }
     }
 }
