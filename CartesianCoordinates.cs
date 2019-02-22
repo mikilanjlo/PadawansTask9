2457a109ae2da342398388587b18a8ed536d4012
mikilanjlo <mikilanjlo@yandex.by>
2019-02-22 11:18:08 +0300

    +argument excepion on all points

--- CartesianCoordinates.cs
+++ CartesianCoordinates.cs
@@ -9,7 +9,7 @@ namespace PadawansTask9
         {
             if (point.Equals(null))
                 throw new ArgumentNullException();
-            if (range < 0)
+            if (range <= 0)
                 throw new ArgumentException();
             List<Point> result = new List<Point>();
             GetNeighbors(point, range,  result, points);
@@ -19,8 +19,12 @@ namespace PadawansTask9
         private static void GetNeighbors(Point neighbor, int range, List<Point> result, params Point[] points)
         {
             foreach (Point point in points)
+            {
+                if (point.Equals(null))
+                    throw new ArgumentNullException();
                 if (Math.Abs(neighbor.X - point.X) <= range && Math.Abs(neighbor.Y - point.Y) <= range)
                     result.Add(point);
+            }
         }
     }
 }
